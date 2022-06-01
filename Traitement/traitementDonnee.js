d53712ce0fde6536cad07a6949524ef6a0720f0d
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-06-01 11:27:58 -0300

    update traitement

--- Traitement/traitementDonnee.js
+++ Traitement/traitementDonnee.js
@@ -21,7 +21,6 @@ for (let i = 0; i < header.length; i++) {
         }
     }
 }
-
 // Traitement des données
 let data = []
 
@@ -31,16 +30,41 @@ for (let i = 1; i < lbreak.length; i++) {
     ligne.shift()
     ligne.pop()
     let document = new Object()
-
     for (let j = 0; j < ligne.length; j++) {
-        if (header[j] == 'key' || header[j] == 'item_type' || header[j] == 'publication_year' || header[j] == 'author' || header[j] == 'title' || header[j] == 'publication_title' || header[j] == 'issn' || header[j] == 'doi' || header[j] == 'url' || header[j] == 'abstract_note' || header[j] == 'date_added' || header[j] == 'date_modified' || header[j] == 'pages' || header[j] == 'language' || header[j] == 'tags') {
+        if (header[j] == 'key' || header[j] == 'item_type' || header[j] == 'publication_year' || header[j] == 'author' || header[j] == 'title' || header[j] == 'publication_title' || header[j] == 'issn' || header[j] == 'doi' || header[j] == 'url' || header[j] == 'abstract_note' || header[j] == 'date_added' || header[j] == 'date_modified' || header[j] == 'pages' || header[j] == 'language' || header[j] == 'automatic_tags') {
+            // Traitement auteurs
+            let arrayOfAuthors = []
+            if (header[j] == 'author') {
+                ligne[j] = ligne[j].split(";")
+                for (let k = 0; k < ligne[j].length; k++) {
+                    if (ligne[j][k].includes(' ')) {
+                        ligne[j][k] = ligne[j][k].replace(' ', '')
+                    }
+                    let author = new Object()
+                    ligne[j][k] = ligne[j][k].split(",")
+                    ligne[j][k]
+                    author.lastName = ligne[j][k][0]
+                    author.firstName = ligne[j][k][1]
+                    arrayOfAuthors.push(author)
+                }
+                ligne[j] = arrayOfAuthors
+
+            }
+            // Traitement des tags
+            if (header[j] == 'automatic_tags') {
+                ligne[j] = ligne[j].split(';')
+                for (let k = 0; k < ligne[j].length; k++) {
+                    if (ligne[j][k].includes(' ')) {
+                        ligne[j][k] = ligne[j][k].replace(' ', '')
+                    }
+                }
+            }
             document[header[j]] = ligne[j]
             data.push(document)
         }
     }
 }
-
-const documents = {documents : data}
+const documents = { documents: data }
 
 // Création du fichier json
 let datas = JSON.stringify(documents)
