7460d7eec90c3cb21727c32400b44b03a4854107
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-06-01 12:14:43 -0300

    Add clean duplicate data

--- Traitement/traitementDonnee.js
+++ Traitement/traitementDonnee.js
@@ -1,4 +1,3 @@
-const { ifError } = require('assert')
 const fs = require('fs')
 const dataCsv = fs.readFileSync('./ZOTEROCSV.csv', 'utf8')
 
@@ -33,22 +32,23 @@ for (let i = 1; i < lbreak.length; i++) {
     for (let j = 0; j < ligne.length; j++) {
         if (header[j] == 'key' || header[j] == 'item_type' || header[j] == 'publication_year' || header[j] == 'author' || header[j] == 'title' || header[j] == 'publication_title' || header[j] == 'issn' || header[j] == 'doi' || header[j] == 'url' || header[j] == 'abstract_note' || header[j] == 'date_added' || header[j] == 'date_modified' || header[j] == 'pages' || header[j] == 'language' || header[j] == 'automatic_tags') {
             // Traitement auteurs
-            let arrayOfAuthors = []
             if (header[j] == 'author') {
+                let arrayOfAuthors = []
                 ligne[j] = ligne[j].split(";")
                 for (let k = 0; k < ligne[j].length; k++) {
                     if (ligne[j][k].includes(' ')) {
                         ligne[j][k] = ligne[j][k].replace(' ', '')
                     }
+                }
+                // Array of authors in string clean
+                for (let k = 0; k < ligne[j].length; k++) {
                     let author = new Object()
-                    ligne[j][k] = ligne[j][k].split(",")
-                    ligne[j][k]
-                    author.lastName = ligne[j][k][0]
-                    author.firstName = ligne[j][k][1]
+                    ligne[j][k] = ligne[j][k].split(',')
+                    author.firstName = ligne[j][k][0]
+                    author.lastName = ligne[j][k][1]
                     arrayOfAuthors.push(author)
                 }
                 ligne[j] = arrayOfAuthors
-
             }
             // Traitement des tags
             if (header[j] == 'automatic_tags') {
@@ -64,7 +64,12 @@ for (let i = 1; i < lbreak.length; i++) {
         }
     }
 }
-const documents = { documents: data }
+
+// Clean duplicate
+let uniq = data => [...new Set(data)]
+const cleanData = uniq(data)
+
+const documents = { documents: cleanData }
 
 // Création du fichier json
 let datas = JSON.stringify(documents)
